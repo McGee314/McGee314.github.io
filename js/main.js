3d5a0e4f42db68cdbec3f3fe0fb8d5c1fd9b5438
McGee314 <samuderabagja@gmail.com>
2024-06-18 21:35:11 +0700

    commit changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,54 @@
- const mntoggle = document.querySelector('.menu-toggle input');
- const nav = document.querySelector('nav ul');
+document.addEventListener("DOMContentLoaded", function() {
+    const mntoggle = document.querySelector('.menu-toggle input');
+    const nav = document.querySelector('nav ul');
 
-mntoggle.addEventListener('click',function(){
-    nav.classList.toggle('menushow');
+    mntoggle.addEventListener('click', function() {
+        nav.classList.toggle('menushow');
+    });
 
-    $(function(){
-        $.get(.../pertemuan_8/headline.json, function(obj){
-            var headline=obj.split(`\n`);
-            var str=``;
+    function updateHeadline() {
+        $.get('./pertemuan_8/headline.json', function(obj) {
+            const headlines = obj.split('\n');
+            let str = '';
 
-            for (var i=0; i<headline.length; i++){
-                str+=${headline[i]}+'</br>';
+            for (let i = 0; i < headlines.length; i++) {
+                str += `${headlines[i]}<br>`;
             }
-            $('#headline').html(str);
-    })
-})
-})
+            $('#headline_text').html(str);
+        });
+    }
+
+    function addItems(kat, jud, pub, up) {
+        const table = document.getElementsByTagName('table')[0];
+        const newRow = table.insertRow(1);
+
+        const cel1 = newRow.insertCell(0);
+        const cel2 = newRow.insertCell(1);
+        const cel3 = newRow.insertCell(2);
+        const cel4 = newRow.insertCell(3);
+
+        cel1.innerHTML = kat;
+        cel2.innerHTML = jud;
+        cel3.innerHTML = pub;
+        cel4.innerHTML = up;
+    }
+
+    function fetchJSONData() {
+        fetch("./info.json")
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                data.forEach(item => {
+                    addItems(item.kategori, item.judul, item.waktu, item.update);
+                });
+            })
+            .catch(error => console.error("Unable to fetch data:", error));
+    }
+
+    updateHeadline();
+    fetchJSONData();
+});
--- python/scrap.py
+++ python/scrap.py
@@ -1,45 +1,37 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 import json
-import re
+from datetime import date
 from datetime import datetime
-import subprocess
+#req ke website
+page = requests.get("https://www.republika.co.id/")
 
-def commit_and_push_changes():
-    subprocess.run(["git", "add", "."])
-    subprocess.run(["git", "commit", "-m", "Update data"])
-    subprocess.run(["git", "push"])
+# Extract konten menjadi objek BeautifulSoup
+soup =  BeautifulSoup(page.text, 'html.parser')#seluruh data web dalam bentuk html
 
-html = requests.get('https://news.republika.co.id/')
-soup = BeautifulSoup(html.text, 'html.parser')
+#mempersempit data pada ruang lingkup class list-group wrap-latest, masih dalam format html
+rawData = soup.find(class_='list-group wrap-latest')
+#mempersempit data pada ruang lingkup class caption, sudah dalam format string
+rawCapts = rawData.find_all(class_='caption')
+#mempersempit data yang ada pada rawData dalam ruang lingkup class date, sudah dalam format string
+rawCatsNDates = rawData.find_all(class_='date')
 
-infoNews = []
-infoNewsSpecial = []
+#transformasi data menjadi array dan memisahkan antara data yang berisikan kategori dan tanggal dengan judul
+rawCapts = [capts.text.strip().split('\n') for capts in rawCapts]
+#transformasi data menjadi array dengan memisahkan antara data yang berisikan kategori dan data yang berisikan tanggal
+CatsNDates = [date.text.strip().split("-") for date in rawCatsNDates]
+#mengambil data-data yang berisikan judul pada rawCaption
+capts = [capt[1] for capt in rawCapts]
+#mengambil data-data yang berisikan kategori pada CatsNDates
+cats = [cat[0] for cat in CatsNDates]
+#mengambil data-data yang berisikan waktu penayangan pada CatsNDates
+dates = [date [1] for date in CatsNDates]
+#variabel yang nantinya menampung data-data berita dalam bentuk yang sama dengan format json
+today = date.today()#mengambil tanggal terkini
+thisTime = datetime.now()#mengambil waktu terkini
+jsonForm = []
+for i in range(0,len(dates),1):
+    jsonForm.append({"kategori":cats[i],"judul":capts[i],"waktu":dates[i],"update":str(today)+" | "+thisTime.strftime("%H:%M")})#memasukkan setiap data kedalam penampung
 
-for i in soup.find_all('div', class_='caption'):
-    time_element = i.find_next('div', class_='date')   # mencari elemen waktu publikasi
-    waktu_publish = (time_element.text.split('-')[1].strip() if time_element
-                     else (i.find("small").text.strip() if i.find("small")
-                           else 'Unknown'))
-    kategori = (i.find_next('span', class_='kanal-info').text if i.find_next('span', class_='kanal-info')
-                else 'Terpopuler')
-    judul = (i.find('h3').text.strip() if i.find('h3')
-             else (i.find(class_='title').text.strip() if i.find(class_='title')
-                   else 'Tidak ada judul'))
-    infoNew = {
-        'judul': judul,
-        'kategori': kategori,
-        'waktu_publish': waktu_publish,
-        'waktu_scraping': datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-    }
-    if re.match(r'\w+ , \d{2} \w+ \d{4}, \d{2}:\d{2}', waktu_publish):
-        infoNewsSpecial.append(infoNew)
-    else:
-        infoNews.append(infoNew)
-
-with open('info.json', 'w') as f:
-    json.dump(infoNews, f)
-
-
-
-commit_and_push_changes()
+with open ("dataBerita.json","w") as updateBerita:
+    json.dump(jsonForm,updateBerita)#memasukkan data yang sudah ditampung kedalam json file 
