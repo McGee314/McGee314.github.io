7d6afa02b0caaaa200d1a010360d28019e6eeb37
McGee314 <samuderabagja@gmail.com>
2024-06-18 22:01:26 +0700

    Ngoding di warnet

--- js/main.js
+++ js/main.js
@@ -1,54 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const mntoggle = document.querySelector('.menu-toggle input');
-    const nav = document.querySelector('nav ul');
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('info.json')
+        .then(response => response.json())
+        .then(data => {
+            const newsTableBody = document.getElementById('news-table-body');
 
-    mntoggle.addEventListener('click', function() {
-        nav.classList.toggle('menushow');
-    });
+            data.forEach(item => {
+                const row = document.createElement('tr');
 
-    function updateHeadline() {
-        $.get('./pertemuan_8/headline.json', function(obj) {
-            const headlines = obj.split('\n');
-            let str = '';
+                const titleCell = document.createElement('td');
+                titleCell.textContent = item.judul;
 
-            for (let i = 0; i < headlines.length; i++) {
-                str += `${headlines[i]}<br>`;
-            }
-            $('#headline_text').html(str);
+                const categoryCell = document.createElement('td');
+                categoryCell.textContent = item.kategori;
+
+                const timeCell = document.createElement('td');
+                timeCell.textContent = item.waktu_publish;
+
+                row.appendChild(titleCell);
+                row.appendChild(categoryCell);
+                row.appendChild(timeCell);
+
+                newsTableBody.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching JSON: ', error);
         });
-    }
-
-    function addItems(kat, jud, pub, up) {
-        const table = document.getElementsByTagName('table')[0];
-        const newRow = table.insertRow(1);
-
-        const cel1 = newRow.insertCell(0);
-        const cel2 = newRow.insertCell(1);
-        const cel3 = newRow.insertCell(2);
-        const cel4 = newRow.insertCell(3);
-
-        cel1.innerHTML = kat;
-        cel2.innerHTML = jud;
-        cel3.innerHTML = pub;
-        cel4.innerHTML = up;
-    }
-
-    function fetchJSONData() {
-        fetch("./info.json")
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error(`HTTP error! Status: ${res.status}`);
-                }
-                return res.json();
-            })
-            .then(data => {
-                data.forEach(item => {
-                    addItems(item.kategori, item.judul, item.waktu, item.update);
-                });
-            })
-            .catch(error => console.error("Unable to fetch data:", error));
-    }
-
-    updateHeadline();
-    fetchJSONData();
 });
